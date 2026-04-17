189ab9285b5a3d5cac07c342038fcd48ae4eaa1d
MuraliMindi <mindi.murali@gmail.com>
2026-04-17 07:56:14 +0000

    added new variable file

---
+++ js-basics/01-variables.js
@@ -0,0 +1,10 @@
+const accountId=7996
+let accountEmail="mindi@test.com"
+var accountPassword="12345"
+accountCity="vizag"
+let accountState
+
+console.log(accountId)
+/*accountId=7997 */ // TypeError: Assignment to constant variable.
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
