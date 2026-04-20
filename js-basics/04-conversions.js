6e813a3481898560610ab188b694f560fd8087f3
MuraliMindi <mindi.murali@gmail.com>
2026-04-20 12:03:17 +0530

    Implement type conversion examples in 04-conversions.js
    Add type conversion examples for various values.

---
+++ js-basics/04-conversions.js
@@ -0,0 +1,21 @@
+let score = 45
+let score-a = 45xyz
+let score-b = null
+let score-c = undefined
+let score-d = true
+let score-e = "murali"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber-a= Number(score-a)
+let valueInNumber-b= Number(score-b)
+let valueInNumber-c= Number(score-c)
+let valueInNumber-d= Number(score-d)
+let valueInNumber-e= Number(score-e)
+console.log(typeof valueInNumber);
+console.log(valueInNumber-a); ///NaN//
+console.log(valueInNumber-b); ///Null//
+console.log(valueInNumber-c); ///Undefined//
+console.log(valueInNumber-d); ///Boolean//
+console.log(valueInNumber-e); ///Boolean//
