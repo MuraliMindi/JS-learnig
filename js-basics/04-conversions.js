148aac7c99036ed869fc41f1205e4c9a5505637d
MuraliMindi <mindi.murali@gmail.com>
2026-04-20 12:10:51 +0530

    Correct variable names and improve type conversions
    Fix variable naming issues and correct conversion methods.

--- js-basics/04-conversions.js
+++ js-basics/04-conversions.js
@@ -1,3 +1,4 @@
+//Number Conversion//
 let score = 45
 let score-a = 45xyz
 let score-b = null
@@ -13,9 +14,28 @@ let valueInNumber-b= Number(score-b)
 let valueInNumber-c= Number(score-c)
 let valueInNumber-d= Number(score-d)
 let valueInNumber-e= Number(score-e)
-console.log(typeof valueInNumber);
+console.log(typeof valueInNumber)
 console.log(valueInNumber-a); ///NaN//
 console.log(valueInNumber-b); ///Null//
 console.log(valueInNumber-c); ///Undefined//
 console.log(valueInNumber-d); ///Boolean//
 console.log(valueInNumber-e); ///Boolean//
+
+//Boolean Conversion//
+let isLoggedIn = 1
+let loginUser = "Murali"
+let loginUser1 = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(boooleanIsLoggedIn) ///Boolean/
+let booleanIsLoginUser = Boolean(loginUser)
+console.log(booleanIsLoginUser) ///Boolean/
+let booleanIsLoginUser1 = Boolean(loginUser1)
+console.log(booleanIsLoginUser1) ///Boolean/
+
+//String Conversion//
+let number = 23
+
+let stringNumber= string (number)
+console.log(stringNumber)
+
